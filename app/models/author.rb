0ed455307b60df924b56315a414f61d1b19db54c
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-03-01 04:21:08 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, uniqueness: true 
+  validates :email, uniqueness: true
   validates :phone_number, length: { minimum: 10 }
   Validates :phone_number, length: { maximum: 10 }
-  
+
 end
